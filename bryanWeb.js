0e562786ce6d085edc6e887931692c038d0c7c56
Bryan Ho <Bryan Ho>
2017-01-10 10:45:52 -0800

    added libraries

--- bryanWeb.js
+++ bryanWeb.js
@@ -2,6 +2,9 @@ var x=30;
 var y=300;
 var jump=0;
 var bottom=true;
+var h=150;
+var b=300;
+
 function setup(){
 
   createCanvas(1080, 400);
@@ -12,9 +15,9 @@ function draw(){
   fill(color(39,199,191));
   rect(x, y, 50, 50);
   y+=jump;
-  if(y==150)
+  if(y==h)
   	jump=10;
-  if(y==300)
+  if(y==b)
   {
   		jump=0;
   		bottom=true;
@@ -27,6 +30,7 @@ function keyPressed()
 	{
 		jump=-10;
 		bottom=false;
+		alert(base+"  "+height);
 	}
 
 }
