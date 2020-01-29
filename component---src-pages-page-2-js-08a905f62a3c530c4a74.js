1f14ff49bb498cbcb3fe7e5269803fb2cfe50347
BryanHo10 <bryankho1010@gmail.com>
2020-01-28 20:11:27 -0800

    Updates

---
+++ component---src-pages-page-2-js-08a905f62a3c530c4a74.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{p5nM:function(e,t,a){"use strict";a.r(t);var n=a("q1tI"),l=a.n(n),o=a("Wbzz"),c=(a("q4sD"),a("Bl7J")),r=(a("tBDR"),a("vrFN"));t.default=function(){return l.a.createElement("div",null,l.a.createElement(c.default,null,l.a.createElement(r.a,{title:"Page two"}),l.a.createElement("h1",null,"Hi from the second page"),l.a.createElement("p",null,"Welcome to page 2"),l.a.createElement(o.Link,{to:"/"},"Go back to the homepage")))}}}]);
+//# sourceMappingURL=component---src-pages-page-2-js-08a905f62a3c530c4a74.js.map
