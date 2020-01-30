9247278a2ed019436f1dde1397d58b105875404f
BryanHo10 <bryankho1010@gmail.com>
2020-01-29 18:09:49 -0800

    Add Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,6 +36,13 @@ module.exports = {
         icon: `src/images/PORTFOLIO-512.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // replace "UA-XXXXXXXXX-X" with your own Tracking ID
+        trackingId: "UA-128030703-1",
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
